40080919f353dabdebe477a868270be76d44b40d
ydgenomics <2144752653@qq.com>
2025-05-20 23:06:13 +0800

    renew 03.plot.R

--- GRN_pySCENIC/03.plot.R
+++ GRN_pySCENIC/03.plot.R
@@ -20,12 +20,12 @@ library(stringr)
 library(circlize)
 
 
+setwd("/data/work/tomato/3.pySCENIC")
 # load data
 loom <- open_loom('aucell.loom')
 
 # get_regulons 函数返回行为转录因子，列为基因的矩阵，值代表该基因是否属于转录因子的靶基因，是的话值为 1，不是则为 0
-regulons_incidMat<-SCopeLoomR::get_regulons(loom,column.attr.name="Regulons")
-
+regulons_incidMat <- SCopeLoomR::get_regulons(loom,column.attr.name="Regulons")
 regulons_incidMat[,1:5]
 #          AL627309.1 AP006222.2 RP11-206L10.2 RP11-206L10.9
 # ACAA1(+)          0          0             0             0
@@ -33,19 +33,21 @@ regulons_incidMat[,1:5]
 # ATF3(+)           0          0             0             0
 # ATF4(+)           0          0             0             0
 
-top2 <- head(rownames(regulons_incidMat),2)
-print(top2)
+#top2 <- head(rownames(regulons_incidMat),2)
+#print(top2)
+gene_list <- rownames(regulons_incidMat)
+print(gene_list)
 
 #regulonsToGeneLists 函数返回每个转录因子调控的靶基因列表
-regulons<-SCENIC::regulonsToGeneLists(regulons_incidMat) 
+regulons <- SCENIC::regulonsToGeneLists(regulons_incidMat) 
 head(regulons,3)
 
 # get_regulons_AUC 返回计算好的转录因子的活性分数矩阵，行为转录因子，列为细胞
-regulonAUC<-SCopeLoomR::get_regulons_AUC(loom,column.attr.name='RegulonsAUC')
+regulonAUC <- SCopeLoomR::get_regulons_AUC(loom,column.attr.name='RegulonsAUC')
 head(regulonAUC)
 
 # get_regulon_thresholds 返回每个转录因子的 AUC 阈值
-regulonAucThresholds<-SCopeLoomR::get_regulon_thresholds(loom)
+regulonAucThresholds <- SCopeLoomR::get_regulon_thresholds(loom)
 head(regulonAucThresholds,3)
 
 embeddings <- SCopeLoomR::get_embeddings(loom)
@@ -53,8 +55,9 @@ embeddings
 close_loom(loom)
 
 # load seurat object
-seu <- readRDS("")
+seu <- readRDS("/data/work/tomato/1.annotation/SixTime_SCT_cellannotation.rds")
 seu
+colnames(seu@meta.data)
 
 Idents(seu) <- "assign.ident"
 
@@ -67,37 +70,21 @@ identical(colnames(sub_regulonAUC),colnames(seu))
 seu@meta.data=cbind(seu@meta.data,t(assay(sub_regulonAUC)))
 
 #############plot
-pdf("./VlnPlot_regulons.pdf", height = 8, width = 12)
+setwd("/data/work/tomato/3.pySCENIC/output")
 
-VlnPlot(
-    seurat.data,
-    features=top2,
-    pt.size=0
-)
-
+pdf("VlnPlot_regulons.pdf", height = 8, width = 12)
+VlnPlot(seu,features=gene_list,pt.size=0)
 dev.off()
 
-pdf("./FeaturePlot_regulons.pdf", height = 8, width = 12)
-
-FeaturePlot(
-    object=seurat.data,
-    features=top2
-)
-
+pdf("FeaturePlot_regulons.pdf", height = 8, width = 12)
+FeaturePlot(object=seu,features=gene_list)
 dev.off()
 
-pdf("./RidgePlot_regulons.pdf", height = 4, width = 8)
-
-RidgePlot(
-    seurat.data,
-    features=top2,
-    ncol=2
-)
-
+pdf("RidgePlot_regulons.pdf", height = 4, width = 8)
+RidgePlot(seu,features=gene_list,ncol=2)
 dev.off()
 
 # 热图可视化
-
 # ==============================================================================
 # AVG expression
 # ==============================================================================
@@ -108,32 +95,28 @@ cellClusters<-data.frame(
 dplyr::mutate(seurat_clusters=ifelse(is.na(seurat_clusters),"unkown",seurat_clusters))
 
 # 这段代码的意思是创建一个名为"cellClusters"的数据框，其中的行名（row.names）是来自于"seurat.data"数据集的列名（colnames），而"seurat_clusters"列则是将"seurat.data$seurat_annotations"转换为字符型数据后的结果。
-
 # 接下来，使用dplyr包中的mutate函数对"seurat_clusters"列进行处理。如果"seurat_clusters"列中的值为NA（缺失值），则将其替换为"unknown"（未知），否则保持不变。最终将处理结果赋值给"seurat_clusters"列。
 
-cellsPerGroup<-split(rownames(cellClusters),cellClusters$seurat_clusters)
+cellsPerGroup <- split(rownames(cellClusters),cellClusters$seurat_clusters)
 
 # remove extened regulons
-sub_regulonAUC<-sub_regulonAUC[onlyNonDuplicatedExtended(rownames(sub_regulonAUC)),]
+sub_regulonAUC <- sub_regulonAUC[onlyNonDuplicatedExtended(rownames(sub_regulonAUC)),]
 
 # Calculate average expression:
-regulonActivity_byGroup<-sapply(
+regulonActivity_byGroup <- sapply(
     cellsPerGroup,
     function(cells)
         rowMeans(getAUC(sub_regulonAUC)[,cells]))
 
 # scale
-regulonActivity_byGroup_Scaled<-t(scale(t(regulonActivity_byGroup),center=T,scale=T))
+regulonActivity_byGroup_Scaled <- t(scale(t(regulonActivity_byGroup),center=T,scale=T))
 
 # heatmap
-pdf("./Heatmap_regulons.pdf", height = 8, width = 8)
-
+pdf("Heatmap_regulons.pdf", height = 8, width = 8)
 Heatmap(matrix=t(regulonActivity_byGroup_Scaled[,]))
-
 dev.off()
 
 # 绘制所有细胞的转录因子活性热图
-
 ht_auc<-assay(sub_regulonAUC)
 ht_auc<-ht_auc[,as.character(unlist(cellsPerGroup))]
 identical(colnames(ht_auc),as.character(unlist(cellsPerGroup)))
@@ -163,8 +146,7 @@ col_anno <- columnAnnotation(celltype = celltypes,
                              col = list(celltype = custom_colors))
 
 # plot
-pdf("./Heatmap_regulons_allcells.pdf", height = 4, width = 8)
-
+pdf("Heatmap_regulons_allcells.pdf", height = 4, width = 8)
 Heatmap(
     matrix = ht_auc_scale,
     top_annotation = col_anno,
@@ -176,7 +158,6 @@ Heatmap(
 dev.off()
 
 # 利用 AUC 计算的阈值，绘制二分类变量热图，也是文献经常见的
-
 # ==============================================================================
 # all cells heatmap
 # ==============================================================================
@@ -199,8 +180,7 @@ at <- match(mark,rownames(binary_tfs))
 tfs_mark = rowAnnotation(foo = anno_mark(at = at,labels=mark))
 
 # plot
-pdf("./Heatmap_Binary_regulons_allcells.pdf", height = 4, width = 8)
-
+pdf("Heatmap_Binary_regulons_allcells.pdf", height = 4, width = 8)
 Heatmap(
     matrix = binary_tfs,
     name = "Binary activity of regulon",
@@ -216,8 +196,7 @@ dev.off()
 
 # 对细胞聚类
 
-pdf("./Heatmap_Binary_regulons_of_allcells.pdf", height = 4, width = 8)
-
+pdf("Heatmap_Binary_regulons_of_allcells.pdf", height = 4, width = 8)
 Heatmap(
     matrix = binary_tfs,
     name = "Binary activity of regulon",
@@ -228,5 +207,5 @@ Heatmap(
 
 dev.off()
 
-Idents(seurat.data)<-"seurat_clusters"
+Idents(seurat.data) <- "seurat_clusters"
 
