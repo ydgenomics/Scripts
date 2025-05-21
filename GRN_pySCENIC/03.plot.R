7d4fe29e0f865dfb865f446c2b6c8160df3d3922
ydgenomics <2144752653@qq.com>
2025-05-21 11:16:04 +0800

    make_option plot.R

--- GRN_pySCENIC/03.plot.R
+++ GRN_pySCENIC/03.plot.R
@@ -1,6 +1,8 @@
-# ==============================================================================
-# plot in R https://mp.weixin.qq.com/s/9n1ITFcC3fT8uyQGlL3Qtw
-# ==============================================================================
+# Title: pySCENIC plot
+# Author: lili, ydgenomics
+# Date: 20250521
+# Image: GRN-SCENIC-R--03 /opt/conda/bin/R
+# Reference: https://mp.weixin.qq.com/s/9n1ITFcC3fT8uyQGlL3Qtw
 library(Seurat)
 library(SCopeLoomR) 
 library(AUCell)
@@ -18,11 +20,22 @@ library(patchwork)
 library(ggplot2)
 library(stringr)
 library(circlize)
+library(optparse)
 
+option_list <- list(
+    make_option(c("-l", "--aucell_loom"), type = "character", default = "/data/work/output/pySCENIC/EFM/aucell.loom",
+                            help = "Path to aucell.loom file", metavar = "character"),
+    make_option(c("-r", "--input_rds"), type = "character", default = "/data/work/output/pySCENIC/EFM/Annotated_EFM_RNA_T_0.5.rds",
+                            help = "Path to input Seurat RDS file", metavar = "character"),
+    make_option(c("-k", "--cluster_key"), type = "character", default = "assign.ident",
+                            help = "Cluster key in Seurat metadata", metavar = "character")
+)
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
-setwd("/data/work/tomato/3.pySCENIC")
 # load data
-loom <- open_loom('aucell.loom')
+#loom <- open_loom('/data/work/tomato/3.pySCENIC/aucell.loom')
+loom <- open_loom(opt$aucell_loom)
 
 # get_regulons 函数返回行为转录因子，列为基因的矩阵，值代表该基因是否属于转录因子的靶基因，是的话值为 1，不是则为 0
 regulons_incidMat <- SCopeLoomR::get_regulons(loom,column.attr.name="Regulons")
@@ -40,7 +53,7 @@ print(gene_list)
 
 #regulonsToGeneLists 函数返回每个转录因子调控的靶基因列表
 regulons <- SCENIC::regulonsToGeneLists(regulons_incidMat) 
-head(regulons,3)
+head(regulons)
 
 # get_regulons_AUC 返回计算好的转录因子的活性分数矩阵，行为转录因子，列为细胞
 regulonAUC <- SCopeLoomR::get_regulons_AUC(loom,column.attr.name='RegulonsAUC')
@@ -48,18 +61,21 @@ head(regulonAUC)
 
 # get_regulon_thresholds 返回每个转录因子的 AUC 阈值
 regulonAucThresholds <- SCopeLoomR::get_regulon_thresholds(loom)
-head(regulonAucThresholds,3)
+head(regulonAucThresholds)
 
 embeddings <- SCopeLoomR::get_embeddings(loom)
 embeddings
 close_loom(loom)
 
 # load seurat object
-seu <- readRDS("/data/work/tomato/1.annotation/SixTime_SCT_cellannotation.rds")
+#seu <- readRDS("/data/work/tomato/1.annotation/SixTime_SCT_cellannotation.rds")
+seu <- readRDS(opt$input_rds)
+seu <- UpdateSeuratObject(seu)
 seu
+#DefaultAssay(seu) <- "RNA"
 colnames(seu@meta.data)
 
-Idents(seu) <- "assign.ident"
+Idents(seu) <- opt$cluster_key
 
 # 把每个转录因子的活性合并到 metadata 里面
 # precoess
@@ -70,7 +86,7 @@ identical(colnames(sub_regulonAUC),colnames(seu))
 seu@meta.data=cbind(seu@meta.data,t(assay(sub_regulonAUC)))
 
 #############plot
-setwd("/data/work/tomato/3.pySCENIC/output")
+#setwd("/data/work/tomato/3.pySCENIC/output1")
 
 pdf("VlnPlot_regulons.pdf", height = 8, width = 12)
 VlnPlot(seu,features=gene_list,pt.size=0)
@@ -88,10 +104,9 @@ dev.off()
 # ==============================================================================
 # AVG expression
 # ==============================================================================
-
-cellClusters<-data.frame(
-    row.names=colnames(seurat.data),
-    seurat_clusters=as.character(seurat.data$assign.ident)) |>
+cellClusters <- data.frame(
+    row.names=colnames(seu),
+    seurat_clusters=as.character(seu@meta.data[[opt$cluster_key]])) |>
 dplyr::mutate(seurat_clusters=ifelse(is.na(seurat_clusters),"unkown",seurat_clusters))
 
 # 这段代码的意思是创建一个名为"cellClusters"的数据框，其中的行名（row.names）是来自于"seurat.data"数据集的列名（colnames），而"seurat_clusters"列则是将"seurat.data$seurat_annotations"转换为字符型数据后的结果。
@@ -103,10 +118,7 @@ cellsPerGroup <- split(rownames(cellClusters),cellClusters$seurat_clusters)
 sub_regulonAUC <- sub_regulonAUC[onlyNonDuplicatedExtended(rownames(sub_regulonAUC)),]
 
 # Calculate average expression:
-regulonActivity_byGroup <- sapply(
-    cellsPerGroup,
-    function(cells)
-        rowMeans(getAUC(sub_regulonAUC)[,cells]))
+regulonActivity_byGroup <- sapply(cellsPerGroup,function(cells)rowMeans(getAUC(sub_regulonAUC)[,cells]))
 
 # scale
 regulonActivity_byGroup_Scaled <- t(scale(t(regulonActivity_byGroup),center=T,scale=T))
@@ -117,13 +129,10 @@ Heatmap(matrix=t(regulonActivity_byGroup_Scaled[,]))
 dev.off()
 
 # 绘制所有细胞的转录因子活性热图
-ht_auc<-assay(sub_regulonAUC)
-ht_auc<-ht_auc[,as.character(unlist(cellsPerGroup))]
-identical(colnames(ht_auc),as.character(unlist(cellsPerGroup)))
-# [1] TRUE
-
-ht_auc_scale<-t(scale(t(ht_auc),center=T,scale=T))
-
+ht_auc <- assay(sub_regulonAUC)
+ht_auc <- ht_auc[,as.character(unlist(cellsPerGroup))]
+identical(colnames(ht_auc),as.character(unlist(cellsPerGroup))) # [1] TRUE
+ht_auc_scale <- t(scale(t(ht_auc),center=T,scale=T))
 # x = 1
 lapply(seq_along(names(cellsPerGroup)),function(x){
     rep(names(cellsPerGroup)[x],length(cellsPerGroup[[x]]))
@@ -136,14 +145,13 @@ colors20 <- c(
   "#999999", "#6BAED6", "#3182BD", "#08519C", "#31A354", "#74C476", "#A1D99B", "#E6550D",
   "#FDAE6B", "#E6550D", "#31A354", "#756BB1"
 )
-length(celltypes)
 unique_celltypes <- unique(celltypes)
+length(unique_celltypes)
 
 # Build a match between cell types and colors
-custom_colors <- setNames(colors20[1:length(unique_celltypes)], unique_celltypes)
+custom_colors <- setNames(colors20[1:length(unique_celltypes)], unique_celltypes) # nolint
 custom_colors
-col_anno <- columnAnnotation(celltype = celltypes, 
-                             col = list(celltype = custom_colors))
+col_anno <- columnAnnotation(celltype = celltypes, col = list(celltype = custom_colors))
 
 # plot
 pdf("Heatmap_regulons_allcells.pdf", height = 4, width = 8)
@@ -162,7 +170,6 @@ dev.off()
 # all cells heatmap
 # ==============================================================================
 ht_auc <- data.frame(ht_auc)
-
 # x = 1
 purrr::map_df(seq_along(regulonAucThresholds),function(x){
     tmp <- ht_auc[regulonAucThresholds[x],]
@@ -170,14 +177,18 @@ purrr::map_df(seq_along(regulonAucThresholds),function(x){
     tmp <- data.frame(apply(tmp, c(1,2), function(x) ifelse(x > val,1,0)))
                             return(tmp)
 }) -> binary_tfs
-
 binary_tfs <- binary_tfs[,as.character(unlist(cellsPerGroup))]
 identical(colnames(binary_tfs),as.character(unlist(cellsPerGroup)))
 
-mark <- sample(rownames(binary_tfs),size = 3,replace = F)
-at <- match(mark,rownames(binary_tfs))
-
-tfs_mark = rowAnnotation(foo = anno_mark(at = at,labels=mark))
+# Random select three rows do annotation
+#mark <- sample(rownames(binary_tfs),size = 3,replace = F)
+#at <- match(mark,rownames(binary_tfs))
+#tfs_mark = rowAnnotation(foo = anno_mark(at = at,labels=mark))
+                      
+# Do annotation for all genes
+mark <- rownames(binary_tfs)
+at <- match(mark, rownames(binary_tfs))
+tfs_mark <- rowAnnotation(foo = anno_mark(at = at, labels = mark))
 
 # plot
 pdf("Heatmap_Binary_regulons_allcells.pdf", height = 4, width = 8)
@@ -195,17 +206,14 @@ Heatmap(
 dev.off()
 
 # 对细胞聚类
-
 pdf("Heatmap_Binary_regulons_of_allcells.pdf", height = 4, width = 8)
 Heatmap(
     matrix = binary_tfs,
     name = "Binary activity of regulon",
     col = c("white", "black"),
     top_annotation = col_anno,
+    right_annotation = tfs_mark,
     show_row_names = F,
     show_column_names = F)
 
 dev.off()
-
-Idents(seurat.data) <- "seurat_clusters"
-
