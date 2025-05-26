e2eb385d347106b2ee0c11bccba6be1097c37d41
ydgenomics <ydgenomics@gmail.com>
2025-05-26 12:35:49 +0800

    update get_AT.file.sh

--- GRN-pySCENIC/create_cistarget_database/get_AT.file.sh
+++ GRN-pySCENIC/create_cistarget_database/get_AT.file.sh
@@ -1,5 +1,5 @@
 # Title: get_AT.file.sh
-# Date: 20250522
+# Date: 20250526
 # Coder: ydgenomics
 # Description: Deal meme file gets motif information and deal txt file gets tbl file including tf_motif information
 # Input: meme and txt files come from PlantTFDB
@@ -8,8 +8,9 @@
 # Reference: https://mp.weixin.qq.com/s/7-vKrLiFS4Tlkt-rHxEGeQ; https://github.com/aertslab/create_cisTarget_databases
 
 # Download .meme and .txt files of Arabidopsis thaliana from PlantTFDB https://planttfdb.gao-lab.org/
-input_txt="/data/work/0.peanut/GRN/input/Ath_TF_binding_motifs_information.txt"
-input_meme="/data/work/0.peanut/GRN/input/Ath_TF_binding_motifs.meme"
+input_txt="/data/work/0.peanut/GRN/AT/Ath_TF_binding_motifs_information.txt"
+input_meme="/data/work/0.peanut/GRN/AT/Ath_TF_binding_motifs.meme"
+species="AT"
 
 ########## Deal txt file get tbl file including tf_motif information ##########
 head -n 5 $input_txt
@@ -24,7 +25,7 @@ NR > 1 {  # 跳过表头行
     matrix_id = $3
     # 打印目标格式
     print matrix_id "\t" matrix_id "\t" gene_id "\tPlantTFDB\t5.0\t" gene_id "\t0\tNone\tNone\t1\tNone\tNone\tgene is directly annotated"
-}' $input_txt > TF_binding_motifs_information.tbl
+}' $input_txt > "$species"_TF_binding_motifs_information.tbl
 
 ########## Deal meme file get motif information ##########
 #less -S $input_meme
@@ -33,14 +34,17 @@ head -n 10 $input_meme
 grep -E "MOTIF|^[[:space:]]*[0-9]" $input_meme | sed 's/MOTIF />/g' | sed 's/^[[:space:]]*//g' > tf_motif_matrix.txt
 ## 替换空格
 sed -i 's/>MA\([0-9.]*\) />MA\1_/' tf_motif_matrix.txt
-less tf_motif_matrix.txt
+head tf_motif_matrix.txt
 ## 保证名字为motif1的名字
-awk '/^>/ {print ">" $2; next} {print}' tf_motif_matrix.txt > formatted_tf_motif_matrix.txt
-less formatted_tf_motif_matrix.txt
+awk '/^>/ {print ">" $2; next} {print}' tf_motif_matrix.txt > tf_motif_matrix2.txt
+head tf_motif_matrix2.txt
 ## 输出保存到每个文件
-mkdir motif_dir
-cd motif_dir
-awk '/^>/{if(file) close(file); filename=substr($0,2)".cb"; print $0 > filename; file=filename; next} {print >> file}'  ../formatted_tf_motif_matrix.txt
+mkdir "$species"_motif_dir
+cd "$species"_motif_dir
+awk '/^>/{if(file) close(file); filename=substr($0,2)".cb"; print $0 > filename; file=filename; next} {print >> file}'  ../tf_motif_matrix2.txt
 ## 准备每个motif id文件
-grep ">" ../formatted_tf_motif_matrix.txt|sed 's/>//g' > ../motifs_id.txt
-head ../motifs_id.txt
+grep ">" ../tf_motif_matrix2.txt|sed 's/>//g' > ../"$species"_motifs_id.txt
+head ../"$species"_motifs_id.txt
+
+rm ../tf_motif_matrix2.txt
+rm ../tf_motif_matrix.txt
---
+++ GRN-pySCENIC/wdl/wdl.md
@@ -0,0 +1,13 @@
+构建跑拟南芥的文件
+
+拟南芥文件准备，放在AT文件夹里面
+```shell
+ll /data/work/0.peanut/GRN/AT
+```
+>total 2673688
+>-r-xr-x--- 1 stereonote 165636  153217903 May 26 12:11 arahy.Tifrunner.gnm2.ann2.PVFB.gene_models_main.gtf
+>-r-xr-x--- 1 stereonote 165636 2583002981 May 26 12:10 arahy.Tifrunner.gnm2.J5K5.genome_main.fa
+>-rw-r--r-- 1 stereonote 165636    1002916 May 26 12:10 Ath_pep.fas
+>-rw-r--r-- 1 stereonote 165636      45049 May 26 12:10 Ath_TF_binding_motifs_information.txt
+>-rw-r--r-- 1 stereonote 165636     505496 May 26 12:10 Ath_TF_binding_motifs.meme
+>-rw-r--r-- 1 stereonote 165636      63427 May 26 12:10 Ath_TF_list.txt
