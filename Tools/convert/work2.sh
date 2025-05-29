1aeac3ea5486ee39740ebea36fea1a755f6ff0cc
ydgenomics <ydgenomics@gmail.com>
2025-05-29 10:34:54 +0800

    1

--- Tools/convert/work2.sh
+++ Tools/convert/work2.sh
@@ -9,10 +9,14 @@ if [ "$ext" == "rds" ]; then
     echo "Converting rds to h5ad..."
     /software/conda/Anaconda/bin/Rscript /script/convert_rdsAh5ad2.R --input_file $input_file --layers all
     python /script/deal_layers_ydgenomics.py --input_path $input_file --sctype $ext
+    echo "Copying rds file..."
+    cp "$input_file" ./
 elif [ "$ext" == "h5ad" ]; then
     echo "Converting h5ad to rds..."
     python /script/deal_layers_ydgenomics.py --input_path $input_file --sctype $ext
     /software/conda/Anaconda/bin/Rscript /script/convert_rdsAh5ad2.R --input_file $input_file --layers all
+    echo "Copying h5ad file..."
+    cp "$input_file" ./
 else
     echo "Error: Unsupported file extension '$ext'. Only 'rds' and 'h5ad' are supported."
 fi
